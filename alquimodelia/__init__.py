20a4543eaaf067f0fc0574c55658784511cad178
João Santos <joao.passagemsantos@edp.pt>
2024-01-17 16:16:10 +0000

    Initial transfer

---
+++ alquimodelia/__init__.py
@@ -0,0 +1,17 @@
+from alquimodelia.archs import (
+    CNNArch,
+    DenseArch,
+    EncoderDecoder,
+    LSTMArch,
+    Transformer,
+    UNETArch,
+)
+
+__all__ = [
+    "CNNArch",
+    "DenseArch",
+    "LSTMArch",
+    "UNETArch",
+    "EncoderDecoder",
+    "Transformer",
+]
